33857e09fda6aff810dbaaa5bee496cf46501e45
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-09-25 10:14:47 +0200

    install webpack

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './style.css';
+import Icon from './icon.png';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+    module: {
+    rules: [
+        {
+            test: /\.js$/i,
+            use: 'val-loader',
+        },
+        {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        },
+        {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        },
+        {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        },            
+    ],
+  },
+};
